e0c8ec87786e64946479a5db32066a56936a1c66
Remah Jubran <jubranodesign@gmail.com>
2023-09-24 13:20:18 +0300

    update css and nav bar scroll

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -5,25 +5,41 @@ import { useEffect, useState } from "react";
 
 export default function Nav(props: { links: INav[] | undefined, isNavFixed?: boolean | false, position?: string | 'top' }) {
     const location = useLocation();
-    const [navCss, setnavCss] = useState<string>('');
+    const [navClass, setNavClass] = useState<string>('');
+    const [lastPosition, setLastPosition] = useState<number>(0);
 
     useEffect(() => {
 
         if (props.isNavFixed) {
             if (props.position === 'top') {
-                setnavCss('NavBar fixed top');
+                setNavClass('NavBar fixed top');
             } else {
-                setnavCss('NavBar fixed bottom');
+                setNavClass('NavBar fixed bottom hide');
             }
         } else {
-            setnavCss('NavBar')
+            setNavClass('NavBar')
         }
-
     }, [])
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+        return () => window.removeEventListener('scroll', handleScroll)
+    }, [lastPosition])
+
+    function handleScroll() {
+        if (props.isNavFixed && props.position === 'bottom') {
+            if (lastPosition > 20 && navClass === 'NavBar fixed bottom hide') {
+                setNavClass('NavBar fixed bottom');
+            } else if (lastPosition < 20 && navClass === 'NavBar fixed bottom') {
+                setNavClass('NavBar fixed bottom hide');
+            }
+            setLastPosition(document.documentElement.scrollTop)
+        }
+    }
 
     return (
         <div>
-            <nav className={navCss}>
+            <nav className={navClass}>
                 {props.links?.map((curr) => (<Link key={curr.id} className={curr.url === location.pathname ? 'nav-item active' : 'nav-item'} to={curr.url}> {curr.name} </Link>))}
             </nav>
         </div>
